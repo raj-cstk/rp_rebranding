f3a7f9b8e420ef17aa33ede9f84f6fcf55ce36e4
Tushar Mittal <tushar.mittal@contentstack.com>
2025-12-19 20:09:43 +0530

    rpcommerce updated

--- src/lib/rpcommerce.js
+++ src/lib/rpcommerce.js
@@ -1,11 +1,12 @@
 const baseURL =  `${process.env.RED_PANDA_COMMERCE_API_URL}/api/public/store/${process.env.RED_PANDA_COMMERCE_STORE_ID}`;
+const headers = {
+    'authorization': process.env.RED_PANDA_COMMERCE_STORE_TOKEN
+}
 
 const RPCommerce = {
     getCategoryByURL: async (url, locale, includeSubCategories = true, level = 2) => {
         const response = await fetch(`${baseURL}/categories/categoryByUrl?url=${url}&includeSubCategories=${includeSubCategories}&locale=${locale}&level=${level}`, {
-            headers: {
-                'authorization': process.env.RED_PANDA_COMMERCE_STORE_TOKEN
-            }
+            headers: headers
         });
         if (!response.ok) {
             return null;
@@ -17,9 +18,7 @@ const RPCommerce = {
 
     getProductsByCategory: async (categoryId, locale) => {
         const response = await fetch(`${baseURL}/products?includeTags=true&includeCategories=true&categories=${categoryId}&includeVariants=true&locale=${locale}&includeMedia=true&includeAttributes=true`, {
-            headers: {
-                'authorization': process.env.RED_PANDA_COMMERCE_STORE_TOKEN
-            }
+            headers: headers
         });
         if (!response.ok) {
             return null;
@@ -31,9 +30,7 @@ const RPCommerce = {
 
     getCategoryFilters: async (categoryId, locale) => {
         const response = await fetch(`${baseURL}/filters/categories/${categoryId}?locale=${locale}`, {
-            headers: {
-                'authorization': process.env.RED_PANDA_COMMERCE_STORE_TOKEN
-            }
+            headers: headers
         });
         if (!response.ok) {
             return null;
